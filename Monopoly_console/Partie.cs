46dd56dc85e6e68774e445c5396632c9343e6209
Camillet21 <ctartare@ensc.fr>
2016-05-09 18:32:30 +0200

    Add files via upload

--- Monopoly_console/Partie.cs
+++ Monopoly_console/Partie.cs
@@ -30,44 +30,43 @@ namespace monopoly
         {
             Plateau = new Plateau();
             Plateau.initPlateau();
-
+            De = new De();
             initJoueurs();
         }
 
         private void initJoueurs()
         {
+           
             Joueurs = new List<Joueur>();
             Console.Write("Entrez le nombre de joueurs s'il vous plaît : ");
             int nbJoueurs = int.Parse(Console.ReadLine());
             for (int i = 0; i < nbJoueurs; i++)
             {
-                Console.Write("\nEntrez le nom du joueur {0} : ", i+1);
+                Console.Write("\nEntrez le nom du joueur {0} : ", i + 1);
                 String s = Console.ReadLine();
+                Joueur J=new Joueur(s);
+                Joueurs.Add(J);
+            }
+            Console.WriteLine("Veuillez lancer les dés pour commencer");
+
+            for (int i = 0; i < nbJoueurs; i++)
+            {   
+                De.Lancerde();
+                Console.Write("\n\n "+Joueurs[i].Nom+ " Votre résultat au dé est de {0}", De.de1 + De.de2);
+              
             }
         }
 
         public void serialiser(XElement racine)
         {
+            XElement partie = new XElement("Partie");
             /*XElement nom = new XElement("Nom", Nom);
             XElement vole = new XElement("Vole", Vole);
             XElement tailleOeufs = new XElement("TailleOeufs", TailleOeufs);
             XElement pays = new XElement("Pays", Pays);
 
             partie.Add(nom, vole, tailleOeufs, pays);*/
-            
-            
-            
-            
-            XElement partie = new XElement("Partie");
-            // <Partie></Partie>
-
-            XElement partie2 = new XElement("Partie", "toto");
-            // <Partie>toto</Partie>
-            
             racine.Add(partie);
-            // <root>
-            // <partie></partie>
-            // </root>
         }
 
         public object deserialiser(XElement racine)
